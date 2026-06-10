ba08a2396d6aea982030c7496fe00470b0400423
trafitol <maarten.schaeffers@gmail.com>
2026-06-10 22:11:07 +0200

    Day 3: fizzbuzz with control flow

---
+++ fizzbuzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+for (int i = 1; i < 101; i++) {
+if (i % 15==0) 
+{
+printf("FizzBuzz\n");
+}
+ else if (i % 5==0)
+{
+printf("Buzz\n");
+}
+else if (i % 3==0)
+{
+printf("Fizz\n");
+}
+else {
+printf("%d\n", i);
+}
+
+}
+return 0;
+}
+
